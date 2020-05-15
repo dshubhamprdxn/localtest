0178294e1ee254116852786b25b0078622f76ec6
Shubham Dawkhar <shubhamd@prdxn.com>
2020-05-15 18:06:11 +0530

    initial commit

---
+++ sites/default/files/php/twig/5ebb9af354710_html.html.twig_ckJ4zgKnj1SzmUvUnXmoYcdpl/HimhvFPhMespalYTHsYzR0oU-FUrSngZKBzjNZYP1uQ.php
@@ -0,0 +1,162 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/zircon/templates/html.html.twig */
+class __TwigTemplate_e488170c0f536d5b5eae87fa902fc199450c81de22375ebef2e289fe340ff580 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 2, "raw" => 4, "safe_join" => 5, "t" => 14];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape', 'raw', 'safe_join', 't'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html";
+        // line 2
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["html_attributes"] ?? null)), "html", null, true);
+        echo ">
+  <head>
+    <head-placeholder token=\"";
+        // line 4
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null)));
+        echo "\">
+    <title>";
+        // line 5
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->env->getExtension('Drupal\Core\Template\TwigExtension')->safeJoin($this->env, $this->sandbox->ensureToStringAllowed(($context["head_title"] ?? null)), " | "));
+        echo "</title>
+    <css-placeholder token=\"";
+        // line 6
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null)));
+        echo "\">
+    <js-placeholder token=\"";
+        // line 7
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null)));
+        echo "\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">
+    <meta name=\"HandheldFriendly\" content=\"true\" />
+    <meta name=\"apple-touch-fullscreen\" content=\"YES\" />
+  </head>
+  <body";
+        // line 12
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null)), "html", null, true);
+        echo ">
+    <a href=\"#main-content\" class=\"visually-hidden focusable\">
+      ";
+        // line 14
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Skip to main content"));
+        echo "
+    </a>
+    ";
+        // line 16
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page_top"] ?? null)), "html", null, true);
+        echo "
+    ";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page"] ?? null)), "html", null, true);
+        echo "
+    ";
+        // line 18
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page_bottom"] ?? null)), "html", null, true);
+        echo "
+    <js-bottom-placeholder token=\"";
+        // line 19
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null)));
+        echo "\">
+  </body>
+</html>
+
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/zircon/templates/html.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  105 => 19,  101 => 18,  97 => 17,  93 => 16,  88 => 14,  83 => 12,  75 => 7,  71 => 6,  67 => 5,  63 => 4,  58 => 2,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html{{ html_attributes }}>
+  <head>
+    <head-placeholder token=\"{{ placeholder_token|raw }}\">
+    <title>{{ head_title|safe_join(' | ') }}</title>
+    <css-placeholder token=\"{{ placeholder_token|raw }}\">
+    <js-placeholder token=\"{{ placeholder_token|raw }}\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">
+    <meta name=\"HandheldFriendly\" content=\"true\" />
+    <meta name=\"apple-touch-fullscreen\" content=\"YES\" />
+  </head>
+  <body{{ attributes }}>
+    <a href=\"#main-content\" class=\"visually-hidden focusable\">
+      {{ 'Skip to main content'|t }}
+    </a>
+    {{ page_top }}
+    {{ page }}
+    {{ page_bottom }}
+    <js-bottom-placeholder token=\"{{ placeholder_token|raw }}\">
+  </body>
+</html>
+
+", "themes/zircon/templates/html.html.twig", "C:\\xampp\\htdocs\\shubham\\Drupal_8\\drupal-test2\\themes\\zircon\\templates\\html.html.twig");
+    }
+}
